a9ffc173dc10dfb2a8009b49b504fc229b6db1d2
wangyaochong <1162025261@qq.com>
2018-10-26 21:33:58 +0800

    学习xxl-job

---
+++ learn-xxl-job-scheduler/src/main/java/com/example/learnxxljob/LearnXxlJobApplication.java
@@ -0,0 +1,12 @@
+package com.example.learnxxljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnXxlJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnXxlJobApplication.class, args);
+	}
+}
