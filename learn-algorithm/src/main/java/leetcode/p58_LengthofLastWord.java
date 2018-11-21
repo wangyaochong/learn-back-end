b69f8e3ac60cca3254e2cde5f23e955818bff3a5
wangyaochong <1162025261@qq.com>
2018-11-21 17:44:36 +0800

    .

---
+++ learn-algorithm/src/main/java/leetcode/p58_LengthofLastWord.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class p58_LengthofLastWord {
+    public int lengthOfLastWord(String s) {
+
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        int count = 0;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            i--;
+            count++;
+        }
+        return count;
+    }
+}
