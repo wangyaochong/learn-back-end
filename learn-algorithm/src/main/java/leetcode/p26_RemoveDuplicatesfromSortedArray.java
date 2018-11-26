4408622f3eb96693eff29cd45160da7aaa1600ca
wangyaochong <1162025261@qq.com>
2018-11-26 10:50:57 +0800

    .

--- learn-algorithm/src/main/java/leetcode/p26_RemoveDuplicatesfromSortedArray.java
+++ learn-algorithm/src/main/java/leetcode/p26_RemoveDuplicatesfromSortedArray.java
@@ -35,4 +35,15 @@ public class p26_RemoveDuplicatesfromSortedArray {
         return nums.length-sameCount;
     }
 
+    //这代码真牛！充分利用排好序的属性！
+    public int removeDuplicates3(int[] nums) {
+        int i = 0;
+        for (int n : nums) {
+            if (i < 1 || n > nums[i - 1]) {
+                nums[i] = n;
+                i++;
+            }
+        }
+        return i;
+    }
 }
