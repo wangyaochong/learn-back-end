e9d60de7a2345f4fdb6aebe4965cf7a29a8ea0d1
wangyaochong <1162025261@qq.com>
2018-12-05 10:18:35 +0800

    .

--- learn-algorithm/src/main/java/leetcode/byNumber/p166_FractiontoRecurringDecimal.java
+++ learn-algorithm/src/main/java/leetcode/byNumber/p166_FractiontoRecurringDecimal.java
@@ -2,23 +2,53 @@ package leetcode.byNumber;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class p166_FractiontoRecurringDecimal {
 
-    public String fractionToDecimal(int numerator, int denominator) {
-        int curLend = 1;
-        if (numerator < denominator) {
-            curLend *= 10;
+    public String fractionToDecimal(int nume, int deno) {
+        long numerator = nume;
+        long denominator = deno;
+        if (numerator == 0) {
+            return "0";
         }
-        int mod = numerator % denominator;
-        while (mod != 0) {
-
+        StringBuilder result = new StringBuilder();
+        result.append(((numerator > 0) ^ (denominator > 0)) ? "-" : "");
+        numerator = Math.abs(numerator);
+        denominator = Math.abs(denominator);
+        if (numerator > denominator) {
+            result.append(numerator / denominator);
+            numerator = numerator % denominator;
         }
-        return null;
+        if (numerator == 0) {
+            return result.toString();
+        }
+        if (result.length() == 0 || result.toString().equals("-")) {
+            result.append("0.");
+        } else {
+            result.append(".");
+        }
+        Map<Long, Integer> cache = new HashMap<>();
+        long tmp;
+        while (numerator != 0) {
+            tmp = (numerator * 10) / denominator;
+            Integer integer = cache.get(numerator);
+            if (integer != null) {
+                result.insert(integer, "(");
+                result.append(")");
+                return result.toString();
+            }
+            cache.put(numerator, result.length());
+            result.append(tmp);
+            numerator = (numerator * 10) % denominator;
+        }
+        return result.toString();
     }
 
     @Test
     public void test() {
-
+        System.out.println(fractionToDecimal(0, 1));
     }
 
 
