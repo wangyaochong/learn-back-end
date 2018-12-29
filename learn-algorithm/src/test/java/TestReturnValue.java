05c87710d0ec5bbaecf5c62586c975fdc2aecb44
wangyaochong <1162025261@qq.com>
2018-12-29 15:32:01 +0800

    .

--- learn-algorithm/src/test/java/TestReturnValue.java
+++ learn-algorithm/src/test/java/TestReturnValue.java
@@ -1,5 +1,10 @@
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestReturnValue {
 
     @Test
@@ -7,6 +12,14 @@ public class TestReturnValue {
     }
 
     public String method() {
-        return "sab";
+        return "xxx";
     }
+
+    @Test
+    public void testBigDecimal() {
+        List<BigDecimal> list = new ArrayList<>(Arrays.asList(BigDecimal.valueOf(1.2), BigDecimal.valueOf(2.3), BigDecimal.valueOf(1.5)));
+        list.sort((a, b) -> (b.compareTo(a)));
+        System.out.println(list);
+    }
+
 }
