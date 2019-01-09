7f7fcaa9b11a4e3d9db925a096d9139bd0b2e8c1
wangyaochong <1162025261@qq.com>
2019-01-09 22:24:11 +0800

    .

--- learn-java/src/main/java/java8/stream/TestStream.java
+++ learn-java/src/main/java/java8/stream/TestStream.java
@@ -1,9 +1,17 @@
 package java8.stream;
 
+import entity.User;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestStream {
     @Test
     public void test(){
+
+        List<User> userList = new ArrayList<>();
+        long sum = userList.stream().mapToLong(User::getId).sum();
+        System.out.println(sum);
     }
 }
