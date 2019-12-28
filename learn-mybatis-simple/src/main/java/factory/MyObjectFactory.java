e56d8388185544721b4f7f5ee259b1b024dd0413
wangyaochong <1162025261@qq.com>
2019-12-29 05:31:34 +0800

    多线程编程核心技术，第一卷

--- learn-mybatis-simple/src/main/java/factory/MyObjectFactory.java
+++ learn-mybatis-simple/src/main/java/factory/MyObjectFactory.java
@@ -6,17 +6,16 @@ import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
 
 import java.util.List;
 
-    @Data
-    public class MyObjectFactory extends DefaultObjectFactory {
-        String specialName;
+@Data
+public class MyObjectFactory extends DefaultObjectFactory {
+    String specialName;
 
 
-
-        @Override
+    @Override
     public <T> T create(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs) {
         T t = super.create(type, constructorArgTypes, constructorArgs);
-        if(type.getSimpleName().equals("UserM")){
-            UserM user =(UserM) t;
+        if (type.getSimpleName().equals("UserM")) {
+            UserM user = (UserM) t;
             user.setForObjectFactory("forObjectFactory");
         }
         return t;
