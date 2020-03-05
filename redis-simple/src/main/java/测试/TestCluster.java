2df81ba9dc1291c5265c9a65d58363f5423c44a6
wangyaochong <1162025261@qq.com>
2020-03-06 05:08:05 +0800

    更详细的解释

--- redis-simple/src/main/java/测试/TestCluster.java
+++ redis-simple/src/main/java/测试/TestCluster.java
@@ -10,15 +10,15 @@ import java.util.Set;
 
 public class TestCluster {
     public static void main(String[] args) throws IOException {
-        Jedis jedis = new Jedis("47.92.250.248", 6381);
+        Jedis jedis = new Jedis("www.wangyaochong.com", 6381);
 //        jedis.set("a", "a234");
         Set<HostAndPort> nodes = new HashSet<>();
-        nodes.add(new HostAndPort("47.92.250.248", 6381));
-        nodes.add(new HostAndPort("47.92.250.248", 6382));
-        nodes.add(new HostAndPort("47.92.250.248", 6383));
-        nodes.add(new HostAndPort("47.92.250.248", 6384));
-        nodes.add(new HostAndPort("47.92.250.248", 6385));
-        nodes.add(new HostAndPort("47.92.250.248", 6386));
+        nodes.add(new HostAndPort("www.wangyaochong.com", 6381));
+        nodes.add(new HostAndPort("www.wangyaochong.com", 6382));
+        nodes.add(new HostAndPort("www.wangyaochong.com", 6383));
+        nodes.add(new HostAndPort("www.wangyaochong.com", 6384));
+        nodes.add(new HostAndPort("www.wangyaochong.com", 6385));
+        nodes.add(new HostAndPort("www.wangyaochong.com", 6386));
         JedisCluster cluster = new JedisCluster(nodes);
         cluster.set("test", "testvalue");
         String test = cluster.get("test");
