858303850098064b7e60f6f5791cd8b622cc4ba5
wangyaochong <yaochong.wang@pactera.com>
2021-07-07 09:09:21 +0800

    volatile原子性

--- java/src/main/java/multiThread/volatile_/NoAtom.java
+++ java/src/main/java/multiThread/volatile_/NoAtom.java
@@ -7,7 +7,7 @@ public class NoAtom {
         //        volatile public static int count = 0;
         public static AtomicInteger count = new AtomicInteger(0);//使用原子类不使用synchronized关键词也可以保持同步
 
-        private static void addCount() {//这个地方加上synchronized就能保证最终count=10000，volatile不具有可见性
+        private static void addCount() {//这个地方加上synchronized就能保证最终count=10000，volatile不具有原子性
             for (int i = 0; i < 100; i++) {
 //                count++;
                 count.incrementAndGet();
