fb979f10c5a9ae6d7bebf546e0b8a3ff48d578ec
wangyaochong <yaochong.wang@pactera.com>
2022-06-25 22:48:06 +0800

    测试LinkedHashMap的有序性

--- java/src/main/java/basic/TestHashMap.java
+++ java/src/main/java/basic/TestHashMap.java
@@ -1,8 +1,8 @@
 package basic;
 
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
+import org.junit.Test;
+
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
@@ -12,12 +12,31 @@ import java.util.concurrent.TimeUnit;
  */
 public class TestHashMap {
     public static void main(String[] args) throws InterruptedException {
-        int count=10000000;
+        int count = 10000000;
         Map<String, Object> map = new ConcurrentHashMap<>(count);
-        for (int i = 0 ; i < count ; i++) {
+        for (int i = 0; i < count; i++) {
             map.put(UUID.randomUUID().toString(), new Random().nextDouble());
             System.out.println(map.size());
         }
         TimeUnit.SECONDS.sleep(10000);
+    }
+
+
+    //测试LinkedHashMap的有序性
+    @Test
+    public void testLinkedHashMap() {
+        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("1", "1");
+        linkedHashMap.put("2", "1");
+        linkedHashMap.put("3", "1");
+        linkedHashMap.put("0", "1");
+        System.out.println(linkedHashMap);
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("1", "1");
+        hashMap.put("2", "1");
+        hashMap.put("3", "1");
+        hashMap.put("0", "1");
+        System.out.println(hashMap);
     }
 }
