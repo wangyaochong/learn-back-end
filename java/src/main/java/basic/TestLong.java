c521d588cb0d843f833bfe9e036f260f014e7aef
wyc <wyc>
2022-07-26 15:09:45 +0800

    提交工具类

---
+++ java/src/main/java/basic/TestLong.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class TestLong {
+    public static void main(String[] args) {
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(System.currentTimeMillis()/1000);
+    }
+}
