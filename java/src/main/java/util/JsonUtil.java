ed32d1e15276db94e8e042e2b04cd032b65aba01
wyc <wyc>
2023-02-19 22:40:46 +0800

    测试通过使用Hutool遍历所有json的key

--- java/src/main/java/util/JsonUtil.java
+++ java/src/main/java/util/JsonUtil.java
@@ -1,9 +1,14 @@
 package util;
 
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.util.Set;
 
 @Slf4j
 public class JsonUtil {
@@ -26,5 +31,17 @@ public class JsonUtil {
             log.error("fromJson异常，json=" + json, e);
         }
         return null;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String content = "{\n" +
+                "\t\"a\": 1,\n" +
+                "\t\"b\": 2\n" +
+                "}";
+        JSON parse = JSONUtil.parse(content);
+        JSONObject jsonObject = JSONUtil.parseObj(content);
+        Set<String> strings = jsonObject.keySet();
+        System.out.println(strings);
+
     }
 }
